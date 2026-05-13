8a686721b7a45450d0ba6d2cc9992404fb359afb
Super User <root@localhost.localdomain>
2026-05-13 23:12:46 +0800

    chore: remove unnecessary nojekyll script
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- scripts/nojekyll.js
+++
@@ -1,8 +0,0 @@
-hexo.extend.filter.register('after_generate', function() {
-  const fs = require('fs');
-  const path = require('path');
-  const nojekyll = path.join(hexo.public_dir, '.nojekyll');
-  if (!fs.existsSync(nojekyll)) {
-    fs.writeFileSync(nojekyll, '');
-  }
-});
